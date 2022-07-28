acd08fa391b0a6a5d7d84f7965cb945e49599a17
Shivam Kumar Singh <64185778+stareelledger@users.noreply.github.com>
2022-07-29 00:11:52 +0530

    Delete flightproject directory

--- flightproject/src/main/java/com/spring/flightproject/repository/ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.flightproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.flightproject.models.Schedule;
-
-public interface ScheduleRepository extends JpaRepository<Schedule ,Integer>{
-
-}
